2ff0dc8de806ccf807afe2883f334ab6b74e9ddb
meawpanipa <73675653+meawpanipa@users.noreply.github.com>
2020-11-19 19:26:56 +0700

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>ITF lab</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body style="background-color:#7A2311;">
+        <?php
+
+        $conn = mysqli_init();
+        mysqli_real_connect($conn, 'finnallab.mysql.database.azure.com', 'meawpanipa@finnallab', 'Ben10alienforce', 'itflab', 3306);
+        if (mysqli_connect_errno($conn))
+        {
+            die('Failed to connect to MySQL: '.mysqli_connect_error());
+        }
+
+
+        $name = $_POST['name'];
+        $comment = $_POST['comment'];
+        $link = $_POST['link'];
+        $id = $_REQUEST['edit_id'];
+
+        $sql = "UPDATE guestbook SET Name='$name', Comment='$comment', Link='$link' WHERE id='$id'";
+
+
+        if (mysqli_query($conn, $sql)) {
+            echo "<h2 style='text-align:center; color:white;'>Record has been update</h2>";
+          } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+          }
+
+        mysqli_close($conn);
+        ?>
+
+        <center><a href = "index.php"><button type="button" class="btn btn-info">Continue</button></a></center>
+    </body>
+</html>
